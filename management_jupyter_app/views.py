e183025430b864b6332b59aa265ad93ed36664c0
佐藤太一 <taichi@taichi.local>
2020-11-26 13:07:51 +0900

    本文・タイトル検索、ページネーション追加

--- management_jupyter_app/views.py
+++ management_jupyter_app/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.urls import reverse_lazy
 from django.views import generic
 from .forms import ManagementCreateForm
@@ -7,6 +8,20 @@ from .models import Management
 class IndexView(generic.ListView):
     """トップページ"""
     model = Management
+    paginate_by = 10
+
+    def get_queryset(self):
+        """
+        上書き(並び替え)
+        """
+        queryset = Management.objects.order_by('-created_at')  # created_atを元に並び替え
+        keyword = self.request.GET.get('keyword')
+        if keyword:
+            queryset = queryset.filter(
+                Q(title__icontains=keyword) | Q(description__icontains=keyword)
+            )
+
+        return queryset
 
 
 class CreateView(generic.CreateView):
