211c70736e5537f5f7e35a77fee3fffbfb3e55ee
佐藤太一 <taichi@taichi.local>
2020-11-27 10:21:23 +0900

    UI変更

--- management_jupyter_app/models.py
+++ management_jupyter_app/models.py
@@ -11,7 +11,7 @@ class Management(models.Model):
     title = models.CharField('タイトル', max_length=255)
     description = models.TextField('説明')
     # ファイルのパスがDBに保存される
-    upload = models.FileField('htmlファイル', upload_to='uploads/%Y/%m/%d/')  # /media/uploads/2018/3/20/ファイル名
+    upload = models.FileField('HTMLファイル', upload_to='uploads/%Y/%m/%d/')  # /media/uploads/2018/3/20/ファイル名
     created_at = models.DateTimeField('作成日', auto_now_add=True)  # default=timezone.nowと違い、入力欄は表示されない
     updated_at = models.DateTimeField('更新日', auto_now=True)  # 更新するたびにその日時が格納される
 
